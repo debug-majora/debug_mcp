e79a99d33a789c3a826d5874da1db15d22d3c8bd
Debug Majora <86309481+debug-majora@users.noreply.github.com>
2025-08-04 16:24:46 -0700

    DART IOC Upload

--- domains_ioc.tf
+++ domains_ioc.tf
@@ -1,8 +1,12 @@
+
+# Creating a locals variable list of IOC domains to be used as as the domain block list (testing).
 locals {
   list_global_blocked_domains_ioc = [
-    "malicious.example.com",
-    "badactor.example.org",
-    "super.bad.example.com",
-    "super.good.example.com",
+    "malicious.example.com",         # 20250708-000000 000000 - Part 1/1
+    "badactor.example.org",           # 20250708-000000 000000 - Part 1/1
+    "super.bad.example.com",         # 20250708-000000 000000 - Part 1/1
+    "bad-ioc-testing.com",   
+    "castellanos-ioc.xyz",   
   ]
 }
+
